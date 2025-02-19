c2f7c7d5d8c8abc8dedd08046d6e151b284bf8a6
Curro Campuzano <campuzanocurrogmail.com>
2025-02-19 15:22:34 +0100

    Fix missing export Tracy-Widom

--- src/genomic_offsets/__init__.py
+++ src/genomic_offsets/__init__.py
@@ -1,5 +1,5 @@
 from .Geometric import GeometricGO
 from .GradientForest import GradientForestGO
 from .RDA import RDA, PartialRDA
-from .RidgeLFMM import RidgeLFMM
+from .RidgeLFMM import RidgeLFMM, tracy_widom_statistics, significant_num_factors_tracy_widom
 from .RONA import RONA
